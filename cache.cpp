14dbaf95d7f48fc6d83dd86789ca901885492709
YiFan.Xiao <franki1996@qq.com>
2019-02-20 11:51:51 -0500

    update cache

--- cache.cpp
+++ cache.cpp
@@ -6,3 +6,17 @@
 #include <errno.h>
 #include <sys/types.h>
 
+// given request, check cache has data / time has expired.
+bool check(Request request){
+    
+}
+
+// given httpResponse, update the data from server.
+void cache::update(Request request){
+    
+}
+
+Response cache::returndata(Request request){
+
+}
+
--- cache.h
+++ cache.h
@@ -8,28 +8,31 @@
 #include <algorithm>
 #include <iostream>
 #include <list>
+#include<iterator>
+#include <utility> 
 #include <unordered_map>
 
 
 // cache class, base on LRU.
 class cache {
-private:
-    std::unordered_map<std::string, int> lookup;
-    std::list<int> dataset;
+protected:
+    // uid, Response
+    std::unordered_map<std::string, std::list<std::pair<std::string, Response>>::iterator> lookup;
+    std::list<std::pair<std::string, Response>> dataset;
 
     // given httpResponse, update the data from server.
-    void update();
-    // read header.
-    std::string cache::read_header(socketkeeper & sock);
+    void update(Request request);
+    // given string, check cache has data / time has expired.
+    bool check(Request request);
+
     // revalidate the data.
     httpResponse revalidate(socketkeeper & server_sock,
                             ttpRequest & info,
                             std::unordered_map<std::string, httpResponse>::iterator to_valid);
 public:
-    size_t get_size();
-    // given string, check cache has data / time has expired.
-    check(std::string request);
 
+    // given request, return the response ready to send back.
+    Response returndata(Request request);
 };
 
 #endif
