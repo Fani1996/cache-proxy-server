f7193f010773cf2254b462f89a8bf8f8efe3fe95
qc36 <qc36@duke.edu>
2019-02-21 13:53:08 -0500

    11

--- main.cpp
+++ main.cpp
@@ -8,7 +8,6 @@
 #include "httpresponse.h"
 
 int main(){
-<<<<<<< HEAD
     HttpSocket server_sk("12345");
     server_sk.listen_to(100);
     int client_fd=server_sk.accept_connect();
@@ -17,14 +16,4 @@ int main(){
 
     std::cout<<"enter recieve"<<std::endl;
     this_request.receive(client_sk);
-=======
-    HttpSocket server_sk("50010");
-    server_sk.listen_to(100);
-    server_sk.accept_connect();
-
-    HttpRequest this_request;
-    this_request.receive(server_sk);
-
-    return 0;
->>>>>>> 7a236e3d6747dfbddd63adb87e61ae38c9f64176
 }
