b3017bf613b8fc238721c0b68fe14c1d6259dda8
YiFan.Xiao <franki1996@qq.com>
2019-02-20 20:46:37 -0500

    update recv_chunck method

--- httpbase.h
+++ httpbase.h
@@ -23,7 +23,41 @@ protected:
 
     void parser();
 
-    
+    void recv_header(HttpSocket& sk);
+    void recv_chunk(HttpSocket& sk);
 };
 
 #endif
+
+void httpBase::recv_chunk(HttpSocket& sk){
+    std::string data;
+    while(1){
+        char buffer[2] = {0};
+        sk.recv_msg(buffer, 1, 0);
+
+        data.push_back(buffer[0]);
+        payload.push_back(buffer[0]);
+
+        unsigned long length = 0;
+
+        // check for length.
+        if(buffer[0] == '\n'){ // finish reciving.
+            std::size_t pos = data.find_first_of(" \r");
+            std::string lenstr = data.substr(0, pos);
+            length = strtoul(lenstr.c_str(), NULL, 16);
+
+            if(length == 0){ // finish reading.
+                return;
+            }
+            else{ // storing data into payload.
+                char* contentbuf = new char[length];
+                memset(reinterpret_cast<void*>(contentbuf), 0, length);
+
+                sk.recv_msg(contentbuf, length+2, MSG_WAITALL);
+                payload.append(contentbuf, length);
+
+                data = "";
+            }
+        }
+    }
+}
