ea7ae55ab98cec9e2195171486b2b4497bb68661
YiFan.Xiao <franki1996@qq.com>
2019-02-20 22:41:17 -0500

    update get_port/get_host function in httpRequest class

--- httprequest.h
+++ httprequest.h
@@ -10,6 +10,11 @@
 
 class HttpRequest : public httpBase{
 private:
+    std::string hostname;
+    std::string port;
+
+    void get_host();
+    void get_port();
 
 public:
     void receive(HttpSocket& sk);
