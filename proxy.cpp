72f2591910731db002cf3efc8bc75b65f417f2a1
qc36 <qc36@duke.edu>
2019-02-23 11:50:01 -0500

    1

---
+++ proxy.cpp
@@ -0,0 +1,20 @@
+#include <unistd.h>
+#include <time.h>
+
+#include <string>
+#include <algorithm>
+#include <iostream>
+#include <list>
+#include <iterator>
+#include <utility> 
+#include <unordered_map>
+
+#include "httprequest.h"
+#include "httpresponse.h"
+
+
+void proxy::request_server(HttpRequest& httprequest){
+  
+
+
+}
---
+++ proxy.h
@@ -0,0 +1,27 @@
+#ifndef _PROXY__H
+#define _PROXY__H
+
+#include <unistd.h>
+#include <time.h>
+
+#include <string>
+#include <algorithm>
+#include <iostream>
+#include <list>
+#include <iterator>
+#include <utility> 
+#include <unordered_map>
+
+#include "httprequest.h"
+#include "httpresponse.h"
+
+class proxy{
+ public:
+ private:
+
+
+
+
+};
+
+#endif
