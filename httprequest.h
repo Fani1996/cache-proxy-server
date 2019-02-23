2b32001b0d971338dc299627b9135cef439d6eff
YiFan.Xiao <franki1996@qq.com>
2019-02-23 11:47:23 -0500

    update http request.cpp/request.h

--- httprequest.h
+++ httprequest.h
@@ -21,12 +21,21 @@ public:
 
     // return identifier for request to id the request in cache.
     std::string get_identifier();
-    // receive request.
-    void receive(HttpSocket& sk);
+    // return request's method.
+    std::string get_method();
+    // return request's version.
+    std::string get_version();
+    
     // get hostname from request.
     std::string get_host();
     // get port from request.
     std::string get_port();
+
+    // deal with CONNECT method of the request.
+    void connect(HttpSocket& server, HttpSocket& client);
+    // receive request.
+    void receive(HttpSocket& sk);
+
 };
 
 #endif
