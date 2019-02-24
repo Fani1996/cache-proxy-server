cd827bc8210efb16e0cff1e94365d736e3b7e4ee
YiFan.Xiao <franki1996@qq.com>
2019-02-23 21:28:43 -0500

    clean up the code for http request

--- httprequest.cpp
+++ httprequest.cpp
@@ -67,45 +67,46 @@ void HttpRequest::connect(HttpSocket& server, HttpSocket& client){
 
     client.send_msg(const_cast<char *>(response.c_str()), response.size());
 
-    int client_fd=client.get_fd();
-    int server_fd=server.get_fd();
+    int client_fd = client.get_fd();
+    int server_fd = server.get_fd();
+
     //connect server and client
     while(1){
-      fd_set read_fd;
-      FD_ZERO(&read_fd);
-      int max_fd=client_fd > server_fd ? client_fd:server_fd;
-      FD_SET (client_fd, &read_fd);
-      FD_SET (server_fd, &read_fd);
-      int active=select(max_fd+1,&read_fd,NULL,NULL,NULL);
-      if(active>0){
-	std::cout<<"select active\n"<<std::endl;
-        if(FD_ISSET(client_fd, &read_fd)){
-	  std::cout<<"client active!!!"<<std::endl;
-	  std::vector<char> buffer(8192,0);
-	  //memset(&buffer,0,sizeof(buffer));
-	  int actual_byte=client.recv_msg(&buffer.data()[0],8192,0);
-	  buffer.resize(actual_byte);
-	  std::cout<<"actual byte"<<actual_byte<<std::endl;
-	  //std::cout<<"buffer::"<<(std::string)(buffer)<<std::endl;
-	  //if connect closed
-	  if(actual_byte==0||actual_byte==-1)
-	    break;
-	  server.send_msg(&buffer.data()[0],actual_byte);
-	}
-	if(FD_ISSET(server_fd, &read_fd)){
-	  std::cout<<"server active!!!!"<<std::endl;
-	  std::vector<char> buffer(8192,0);
-          //memset(&buffer,0,sizeof(buffer));
-          int actual_byte=server.recv_msg(&buffer.data()[0],8192,0);
-	   buffer.resize(actual_byte);
-	  std::cout<<"actual byte"<<actual_byte<<std::endl;
-	  // std::cout<<"buffer::"<<(std::string)(buffer)<<std::endl;
-	  //if connect close
-          if(actual_byte==0||actual_byte==-1)
-            break;
-          client.send_msg(&buffer.data()[0],actual_byte);
-	}
-      }
+        fd_set read_fd;
+        FD_ZERO(&read_fd);
+
+        int max_fd = client_fd > server_fd ? client_fd : server_fd;
+        FD_SET (client_fd, &read_fd);
+        FD_SET (server_fd, &read_fd);
+
+        int active = select(max_fd+1, &read_fd, NULL, NULL, NULL);
+        if(active > 0){
+            std::cout<<"select active\n"<<std::endl;
+            if(FD_ISSET(client_fd, &read_fd)){
+                std::cout<<"=== CLIENT Active ==="<<std::endl;
+                std::vector<char> buffer(8192,0);
+
+                int actual_byte = client.recv_msg(&buffer.data()[0], 8192, 0);
+                buffer.resize(actual_byte);
+                std::cout<<"actual byte"<<actual_byte<<std::endl;
+                //if connect closed
+                if(actual_byte == 0 || actual_byte == -1)
+                    break;
+                server.send_msg(&buffer.data()[0], actual_byte);
+            }
+            if(FD_ISSET(server_fd, &read_fd)){
+                std::cout<<"=== SERVER Active ==="<<std::endl;
+                std::vector<char> buffer(8192,0);
+
+                int actual_byte = server.recv_msg(&buffer.data()[0], 8192, 0);
+                buffer.resize(actual_byte);
+                std::cout<<"actual byte"<<actual_byte<<std::endl;
+                //if connect close
+                if(actual_byte == 0 || actual_byte == -1)
+                    break;
+                client.send_msg(&buffer.data()[0], actual_byte);
+            }
+        }
     }
 }
 
