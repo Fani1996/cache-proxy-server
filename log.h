c299e9631ce26451f3b4dc126dcfb25bdce34d7b
YiFan.Xiao <franki1996@qq.com>
2019-02-26 22:12:29 -0500

    update log

--- log.h
+++ log.h
@@ -2,30 +2,38 @@
 #include <ostream>
 #include <istream>
 #include <fstream>
-
-#include <filesystem>
-
+#include <cstdlib>
+#include <unistd.h>
 
 class Log {
 private:
     std::string path;
     std::string filename;
 
+    std::string get_working_path(){
+        std::size_t MAXPATHLEN = 512;
+        char temp[MAXPATHLEN];
+        return ( getcwd(temp, sizeof(temp)) ? std::string( temp ) : std::string("") );
+    }
+
 public:
     Log() {
-        path = std::filesystem::current_path();
+        path = get_working_path();
         filename = "log.txt";
-        system("mkdir " + path + filename);
+
+        std::string target = "mkdir " + path + filename;
+        system(target.c_str());
     }
     Log(std::string path, std::string filename) : path(path), filename(filename) {
-        system("mkdir " + path + filename);
+        std::string target = "mkdir " + path + filename;
+        system(target.c_str());
     }
 
     void output(std::string output){
         std::cin >> output;
         std::ofstream out(path + filename);
         out << output;
-        
+
         out.close();
     }
 };
